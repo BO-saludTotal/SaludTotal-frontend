0d9003b2658028a3191cc9818e6d9b0afdc0a539
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-05-29 19:42:54 -0400

    Agregando servicio de usuario

--- frontend/src/app/paginas/registro/registro.component.ts
+++ frontend/src/app/paginas/registro/registro.component.ts
@@ -1,17 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // <-- AGREGA ESTO
+import { CommonModule } from '@angular/common'; 
+import { UsuarioService } from '../../services/usuario.service';
+
 
 @Component({
   selector: 'app-registro',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-    onSubmit() {
-    console.log('Formulario enviado');
-    // Aquí podrías hacer validaciones o enviar a backend
+usuario = {
+  tipo: '', // <--- nuevo
+  nombreCompleto: '',
+  ci: '',
+  email: '',
+  celular: '',
+  direccion: '',
+  fechaNacimiento: '',
+  genero: '',
+  password: ''
+};
+
+
+  constructor(private usuarioService: UsuarioService) {}
+
+onSubmit() {
+  if (this.validarCampos()) {
+    this.usuarioService.registrarUsuario(this.usuario).subscribe({
+      next: (res: any) => {
+        console.log('Usuario registrado:', res);
+        alert('Usuario registrado con éxito');
+      },
+      error: (err: any) => {
+        console.error('Error al registrar:', err);
+        alert('Hubo un error al registrar');
+      }
+    });
+  } else {
+    alert('Completa todos los campos obligatorios');
   }
+}
 
-
+  validarCampos(): boolean {
+    return Object.values(this.usuario).every(valor => valor !== '');
+  }
 }
