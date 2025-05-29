6738d6657881c38ec3e5bb64e5371792593c9de3
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-05-29 13:43:01 -0400

    mejoras

---
+++ frontend/frontend/src/app/paginas/registro/registro.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  imports: [RouterModule],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+    onSubmit() {
+    console.log('Formulario enviado');
+    // Aquí podrías hacer validaciones o enviar a backend
+  }
+
+
+}
