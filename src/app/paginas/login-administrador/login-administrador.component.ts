eaa16d014d244477318c5ecafd67de7a109ba5bf
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-06-03 15:30:32 -0400

    Agregado los logins y el registro de acuerdo a la api service

--- src/app/paginas/login-administrador/login-administrador.component.ts
+++ src/app/paginas/login-administrador/login-administrador.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login-administrador',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './login-administrador.component.html',
   styleUrl: './login-administrador.component.css'
 })
 export class LoginAdministradorComponent {
-    onSubmit() {
-    console.log("Formulario de administrador enviado");
-  }
+  usuario = {
+    ci: '',
+    codigo: '',
+    password: ''
+  };
 
+  constructor(private api: ApiService, private router: Router) {}
+
+  onSubmit() {
+    if (this.usuario.ci && this.usuario.codigo && this.usuario.password) {
+      this.api.post('auth/login', this.usuario).subscribe({
+        next: (res: any) => {
+          console.log('Login exitoso:', res);
+          localStorage.setItem('token', res.accessToken);
+          this.router.navigate(['/dashboard-administrador']);
+        },
+        error: (err) => {
+          console.error('Error en login:', err);
+          alert('CI, código o contraseña incorrectos');
+        }
+      });
+    } else {
+      alert('Por favor completa todos los campos');
+    }
+  }
 }
