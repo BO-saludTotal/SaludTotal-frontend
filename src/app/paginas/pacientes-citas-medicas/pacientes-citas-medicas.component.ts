98fe1159e0ddf4022a9b2f00bd6f72ab4f8ba1ad
Jhaelin13 <Jhaelin.alvarado@ucb.edu.bo>
2025-06-11 21:22:44 -0400

    Agrega componente PacienteCitasMedicas

---
+++ src/app/paginas/pacientes-citas-medicas/pacientes-citas-medicas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pacientes-citas-medicas',
+  imports: [],
+  templateUrl: './pacientes-citas-medicas.component.html',
+  styleUrl: './pacientes-citas-medicas.component.css'
+})
+export class PacientesCitasMedicasComponent {
+
+}
