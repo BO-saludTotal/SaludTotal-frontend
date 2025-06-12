e5bd4c567cb1000378c8d8259fcc0c55c594e58e
AlejoHuay <alvarez.h.alejo@gmail.com>
2025-06-12 11:31:01 -0400

    ahora ya te muestra tu nom de usuario (carnet) al logearte

--- src/app/paginas/dashboard-paciente/dashboard-paciente.component.ts
+++ src/app/paginas/dashboard-paciente/dashboard-paciente.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,11 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dashboard-paciente.component.html',
   styleUrl: './dashboard-paciente.component.css'
 })
-export class DashboardPacienteComponent {
+export class DashboardPacienteComponent implements OnInit {
+  username: string = 'Paciente';
 
+  ngOnInit() {
+    // history.state.username viene de NavigationExtras.state
+    this.username = history.state.username || this.username;
+  }
 }
--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
+import { NavigationExtras } from '@angular/router'; // nuevo cambio para enviar el nom de usuario al dashborard
 
 @Component({
   selector: 'app-login',
@@ -31,21 +32,26 @@ export class LoginComponent {
           // Detectar correctamente el rol desde diferentes estructuras posibles
           const roleId = res.roleId ?? res.user?.roleId ?? res.data?.roleId;
 
-          console.log('Detectado roleId:', roleId);
+          console.log('Detectado roleId:', roleId); 
+
+          // Creamos un objeto NavigationExtras con nuestro username
+          const extras: NavigationExtras = { // nuevo cambio para enviar el nom de usuario al dashborard
+            state: { username: this.usuario.username }
+          };
 
           // Redirección según el rol
           switch (roleId) {
             case 1:
-              this.router.navigate(['/dashboard-gobierno']);
+              this.router.navigate(['/dashboard-gobierno'], extras); // nuevo cambio pasamos el nuevo obj extras junto con la ruta
               break;
             case 2:
-              this.router.navigate(['/dashboard-paciente']);
+              this.router.navigate(['/dashboard-paciente'], extras);
               break;
             case 3:
-              this.router.navigate(['/dashboard-doctor']);
+              this.router.navigate(['/dashboard-doctor'], extras);
               break;
             case 4:
-              this.router.navigate(['/dashboard-administrador']);
+              this.router.navigate(['/dashboard-administrador'], extras);
               break;
             default:
               alert('Rol no reconocido');
